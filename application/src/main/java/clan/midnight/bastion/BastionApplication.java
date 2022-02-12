affece98023ce197ac2460c7f90ec723e2c8957c
wuweiran <wwrustc@mail.ustc.edu.cn>
2022-02-12 09:10:15 +0800

    Initial commit

---
+++ application/src/main/java/clan/midnight/bastion/BastionApplication.java
@@ -0,0 +1,13 @@
+package clan.midnight.bastion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BastionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BastionApplication.class, args);
+	}
+
+}
